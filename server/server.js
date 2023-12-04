46fd99222f818eee53b8a1fde14968c8c08460f0
amtenu <amannov21@gmail.com>
2023-12-04 08:27:02 -0700

    Packages installed , database connected

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from 'mongoose';
+import { DATABASE } from "./config.js";
+
+
+const app= express();
+
+//db
+
+mongoose
+.connect(DATABASE)
+.then(()=>console.log("db_connected"))
+.catch((err)=>console.log(err))
+
+
+//middleware
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+app.get('/api',(req,res)=>{
+res.json({
+    data:'Hello from nodejs api'
+})
+})
+
+app.listen(8000,()=>console.log(
+    "server running on 8000"
+));
