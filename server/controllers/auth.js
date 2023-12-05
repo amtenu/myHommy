680278b1ac752756145552a7cbbe65a1ac0a4d0b
amtenu <amannov21@gmail.com>
2023-12-05 10:40:42 -0700

    hashed using bcrypt for db

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -43,8 +43,8 @@ export const preRegister = async (req, res) => {
 export const register = async (req, res) => {
   try {
     //console.log(req.body);
-const decoded=jwt.verify(req.body.token,config.JWT_SECRET);
-console.log(decoded)
+const {email,password}=jwt.verify(req.body.token,config.JWT_SECRET);
+//console.log(email,password)
 
   } catch (err) {
     console.log(err);
