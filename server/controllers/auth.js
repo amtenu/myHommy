d193de92c0bca446cf392ca8484706a856a798aa
amtenu <amannov21@gmail.com>
2023-12-04 21:24:22 -0700

    aws SES for pre-registration confirmation email

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+import * as config from "../config.js";
+
 export const welcome = (req, res) => {
   res.json({
     data: "Hello from nodejs api",
@@ -6,14 +8,37 @@ export const welcome = (req, res) => {
 
 export const preRegister = async (req, res) => {
   try {
-    console.log(req.body);
-
-    let emailSent = true;
-    if (emailSent) {
-      return res.json({ ok: true });
-    } else {
-      return res.json({ ok: false });
-    }
+    config.AWSSES.sendEmail(
+      {
+        Source: config.EMAIL_FROM,
+        Destination: {
+          ToAddresses: ["amannugussie@gmail.com"],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: "UTF-8",
+              Data: `
+              <h1>WELCOME TO myHommy</h1>
+              `,
+            },
+          },
+          Subject: {
+            Charset: "UTF-8",
+            Data: "Welcome to myHommy",
+          },
+        },
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.json({ ok: false });
+        } else {
+          console.log(data);
+          return res.json({ ok: true });
+        }
+      }
+    );
   } catch (err) {
     console.log(err);
     return res.json({ error: "Oppppppssssyy ERROR, TRY AGAIN" });
