b606876657c51a9387f3bffb587cba02c904a6b2
amtenu <amannov21@gmail.com>
2023-12-06 08:45:14 -0700

    routers protected --middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,16 @@
 import express from "express"
 const router=express.Router();
  import * as auth from "../controllers/auth.js";
+ import {requireSignin} from "../middlewares/auth.js"
 
 
 
-router.get('/',auth.welcome)
+router.get('/',requireSignin,auth.welcome)
 router.post("/pre-register",auth.preRegister) //Until they validiate with comformation email
 router.post("/register",auth.register)
 router.post("/login",auth.login)
 router.post('/forgotpassword',auth.forgotPassword)
 router.post('/accessaccount',auth.accessaccount)
+
 
 export default router
