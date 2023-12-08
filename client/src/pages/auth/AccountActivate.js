55df68ed8072b8f09449472893deeb6827a561f0
amtenu <amannov21@gmail.com>
2023-12-08 12:22:30 -0700

    activate email link token grabbed

--- client/src/pages/auth/AccountActivate.js
+++ client/src/pages/auth/AccountActivate.js
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom"
 export default function AccountActivate(){
+
+    const params=useParams();
+    const token =params.token
+    console.log(token)
+
     return (
-        <div>
-            Account activate.
+        <div className="display-1 d-flex justify-content-center align-items-center vh-100 " style={{marginTop:"-5%"}}>
+            Please wait .......
         </div>
     )
 
