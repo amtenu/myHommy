cac93375dec3f597ff4d4dc1f8da57dcc43bd914
amtenu <amannov21@gmail.com>
2023-12-08 08:35:14 -0700

    user menu drop down

--- client/src/components/nav/Main.js
+++ client/src/components/nav/Main.js
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { NavLink } from "react-router-dom";
 
 export default function Main() {
   return (
-    <nav className="nav">
+    <nav className="nav d-flex justify-content-between lead">
       <NavLink className="nav-link " aria-current="page" to="/">
         Home
       </NavLink>
@@ -10,8 +11,25 @@ export default function Main() {
         Login
       </NavLink>
       <NavLink className="nav-link" to="/register">
-        Link
+        Register
       </NavLink>
+      <div className="dropdown">
+        <li>
+          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+            User
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <NavLink className="nav-link " to="/dashbord">
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <a className="nav-link">Logout</a>
+            </li>
+          </ul>
+        </li>
+      </div>
     </nav>
   );
 }
