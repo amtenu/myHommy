a3191a712a57e45dcdfb2dfce5c9276dee5f78a3
amtenu <amannov21@gmail.com>
2023-12-09 09:11:57 -0700

    changed route name to align with frontend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ router.get('/',requireSignin,auth.welcome)
 router.post("/pre-register",auth.preRegister) //Until they validiate with comformation email
 router.post("/register",auth.register)
 router.post("/login",auth.login)
-router.post('/forgotpassword',auth.forgotPassword)
+router.post('/forgot-password',auth.forgotPassword)
 router.post('/accessaccount',auth.accessaccount)
 router.get('/referesh-token',auth.refereshToken)
 router.get('/current-user',requireSignin,auth.currentuser)
