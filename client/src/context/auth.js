cb34a0adb30ee5198629b4a60679816cbae2c71b
amtenu <amannov21@gmail.com>
2023-12-11 11:34:04 -0700

    if token expires , referesh-token endpoint recreates token for logged in users from api rather than from sesssion storage

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -19,9 +19,52 @@ const AuthProvider = ({ children }) => {
     }
   }, []);
 
-  //config axios
+  //config axios no need to /API
 
   axios.defaults.baseURL = API;
+  //global config for axios when sending request include the token
+  axios.defaults.headers.common['Authorization']=auth?.token
+  axios.defaults.headers.common['refresh_token']=auth?.refreshToken
+
+  axios.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      const originalConfig = err.config;
+   
+      if (err.response) {
+        // token is expired
+        if (err.response.status === 401 && !originalConfig._retry) {
+          originalConfig._retry = true;
+   
+          try {
+            const { data } = await axios.get("/refresh-token");
+            axios.defaults.headers.common["token"] = data.token;
+            axios.defaults.headers.common["refresh_token"] = data.refreshToken;
+   
+            setAuth(data);
+            localStorage.setItem("auth", JSON.stringify(data));
+   
+            return axios(originalConfig);
+          } catch (_error) {
+            if (_error.response && _error.response.data) {
+              return Promise.reject(_error.response.data);
+            }
+   
+            return Promise.reject(_error);
+          }
+        }
+   
+        if (err.response.status === 403 && err.response.data) {
+          return Promise.reject(err.response.data);
+        }
+      }
+   
+      return Promise.reject(err);
+    }
+  );
+  
 
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
