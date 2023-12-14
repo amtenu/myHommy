d424ea9deab57395cc64b1e8adc3a9a9e6e6d8af
amtenu <amannov21@gmail.com>
2023-12-14 09:24:47 -0700

    fix

--- client/src/pages/user/ad/RentLand.js
+++ client/src/pages/user/ad/RentLand.js
@@ -4,7 +4,7 @@ import AdForm from "../../../components/form/AdForm";
 export default function RentLand() {
   return (
     <div>
-      <h1 className="dispaly-1 bg-primary text-light p-5">Rent Land </h1>
+      <h1 className="display-1 bg-primary text-light p-5">Rent Land </h1>
       <SideBar />
       <div className="container mt-2">
         <AdForm action="Rent" type="Land" />
--- client/src/pages/user/ad/SellHouse.js
+++ client/src/pages/user/ad/SellHouse.js
@@ -4,7 +4,7 @@ import AdForm from "../../../components/form/AdForm";
 export default function SellHouse() {
   return (
     <div>
-      <h1 className="dispaly-1 bg-primary text-light p-5">Sell House </h1>
+      <h1 className="display-1 bg-primary text-light p-5">Sell House </h1>
       <SideBar />
       <div className="container mt-2">
         <AdForm action="Sell" type="House" />
