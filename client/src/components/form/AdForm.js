27d500fb6fd6d81f89c33ee236b2362b65b9e992
amtenu <amannov21@gmail.com>
2023-12-14 12:36:32 -0700

    address can automatically be selected and populated from Google autoplaces

--- client/src/components/form/AdForm.js
+++ client/src/components/form/AdForm.js
@@ -1,27 +1,38 @@
 import { useState } from "react";
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-import {GOOGLE_PLACES_KEY} from '../../config/config'
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { GOOGLE_PLACES_KEY } from "../../config/config";
 
 export default function AdForm({ action, type }) {
-  const [ad,setAd]=useState({
-    photos:[],
-    uploading:false,
-    price:'',
-    address:'',
-    bedroom:'', 
-    bathroom:'',
-    carPark:'',
-    landsize:'',
-    basement:false,
-    garage:false,
-    title:'',
-    description:'',
-    loading:false
-  })
+  const [ad, setAd] = useState({
+    photos: [],
+    uploading: false,
+    price: "",
+    address: "",
+    bedroom: "",
+    bathroom: "",
+    carPark: "",
+    landsize: "",
+    basement: false,
+    garage: false,
+    title: "",
+    description: "",
+    loading: false,
+  });
   return (
     <>
-      <p>This is ad create form</p>
-      {action}/{type}
+      <div className="form-control">
+        <GooglePlacesAutocomplete
+          apiKey={GOOGLE_PLACES_KEY}
+          apiOptions="ca"
+          selectProps={{
+            defaultInputValue: ad?.address,
+            placeholder: "Search for address ...",
+            onChange: ({ value }) =>
+              setAd({ ...ad, address: value.description }),
+          }}
+        />
+      </div>
+      <pre>{JSON.stringify(ad, null, 4)}</pre> 
     </>
   );
 }
