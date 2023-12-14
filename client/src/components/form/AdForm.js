18feedcbc2281b5316dd61936dee3fd55ffece87
amtenu <amannov21@gmail.com>
2023-12-14 13:50:59 -0700

    formatted input currency

--- client/src/components/form/AdForm.js
+++ client/src/components/form/AdForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { GOOGLE_PLACES_KEY } from "../../config/config";
+import CurrencyInput from "react-currency-input-field";
 
 export default function AdForm({ action, type }) {
   const [ad, setAd] = useState({
@@ -32,7 +33,13 @@ export default function AdForm({ action, type }) {
           }}
         />
       </div>
-      <pre>{JSON.stringify(ad, null, 4)}</pre> 
+      <CurrencyInput
+        placeholder="Please Enter price"
+        defaultValue={ad.price}
+        className="form-control mb-3"
+        onValueChange={(value) => setAd({ ...ad, price: value })}
+      />
+      <pre>{JSON.stringify(ad, null, 4)}</pre>
     </>
   );
 }
