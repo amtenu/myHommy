997a999ffe6fd5f33919f3dd230397f73b395239
amtenu <amannov21@gmail.com>
2023-12-14 12:23:39 -0700

    google places key, state management and npm package installed

--- client/src/components/form/AdForm.js
+++ client/src/components/form/AdForm.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import {GOOGLE_PLACES_KEY} from '../../config/config'
+
 export default function AdForm({ action, type }) {
+  const [ad,setAd]=useState({
+    photos:[],
+    uploading:false,
+    price:'',
+    address:'',
+    bedroom:'', 
+    bathroom:'',
+    carPark:'',
+    landsize:'',
+    basement:false,
+    garage:false,
+    title:'',
+    description:'',
+    loading:false
+  })
   return (
     <>
       <p>This is ad create form</p>
