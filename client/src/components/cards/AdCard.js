fa7e908d854f5c94ce5667cec097c44036e1e5e9
amtenu <amannov21@gmail.com>
2023-12-21 15:31:13 -0700

    price is formatted

--- client/src/components/cards/AdCard.js
+++ client/src/components/cards/AdCard.js
@@ -3,6 +3,9 @@ import { TbBath } from "react-icons/tb";
 import { BiArea } from "react-icons/bi";
 
 export default function AdCard({ ad }) {
+  function format(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <div className="col-lg-4 p-4 gx-4 gy-4">
       <div className="Card hoverable shadow">
@@ -14,7 +17,7 @@ export default function AdCard({ ad }) {
       </div>
       <div className="card-body">
         <div className="d-flex justify-content-between">
-          <h3>{ad?.price}</h3>
+          <h3>CAD&nbsp;{format(ad?.price)}</h3>
         </div>
         <p className="card-text d-flex justify-content-between">
           {ad?.bedrooms ? (
@@ -31,7 +34,7 @@ export default function AdCard({ ad }) {
           ) : (
             ""
           )}{" "}
-          {ad?.landsize? (
+          {ad?.landsize ? (
             <span>
               <BiArea /> {ad?.landsize}
             </span>
