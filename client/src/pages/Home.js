5e01be4c18a0e11dab2c8c7bb3f1602d58015030
amtenu <amannov21@gmail.com>
2023-12-21 13:39:06 -0700

    adsforrent and adforsell can be viewd in json format

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,33 @@
-
+import axios from "axios";
 import { useAuth } from "../context/auth";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  const [auth, setAuth] = useAuth();
+  //context from Auth
+  const [auth, setAuth] = useState();
+  //state
+
+  const [adsForSell, setAdsForSell] = useState();
+  const [adsForRent, setAdsForRent] = useState();
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
+
+  const fetchAds = async () => {
+    try {
+      const { data } = await axios.get("/ads");
+      setAdsForSell(data.adsForSell);
+      setAdsForRent(data.adsForRent);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <h1 className="display-1 bg-primary text-light p-5">Home</h1>
-      <pre>{JSON.stringify(auth,null,5)}</pre>
+      <pre>{JSON.stringify({adsForSell,adsForRent}, null, 5)}</pre>
     </div>
   );
 }
