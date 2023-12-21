72bfa8f36aa07dab932f479f24517458cb1c7455
amtenu <amannov21@gmail.com>
2023-12-21 14:24:45 -0700

    users can view Rent functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,6 @@ import { useAuth } from "../context/auth";
 import { useEffect, useState } from "react";
 import AdCard from "../components/cards/AdCard";
 
-
 export default function Home() {
   //context from Auth
   const [auth, setAuth] = useState();
@@ -33,7 +32,17 @@ export default function Home() {
       <div className="container">
         <div className="row">
           {adsForSell?.map((ad) => (
-            <AdCard ad={ad} Key={ad._id}/>
+            <AdCard ad={ad} Key={ad._id} />
+          ))}
+        </div>
+      </div>
+
+      <h1 className="display-1 bg-primary text-light p-5">For Rent</h1>
+      {/*<pre>{JSON.stringify({adsForSell,adsForRent}, null, 5)}</pre>*/}
+      <div className="container">
+        <div className="row">
+          {adsForRent?.map((ad) => (
+            <AdCard ad={ad} Key={ad._id} />
           ))}
         </div>
       </div>
