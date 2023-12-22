9d81825718d433417edc2f65076d128f7d2e5fb7
amtenu <amannov21@gmail.com>
2023-12-21 21:17:19 -0700

    ribbon for user interface

--- client/src/components/cards/AdCard.js
+++ client/src/components/cards/AdCard.js
@@ -1,6 +1,7 @@
 import { IoBedOutline } from "react-icons/io5";
 import { TbBath } from "react-icons/tb";
 import { BiArea } from "react-icons/bi";
+import { Badge } from "antd";
 
 export default function AdCard({ ad }) {
   function format(x) {
@@ -8,41 +9,46 @@ export default function AdCard({ ad }) {
   }
   return (
     <div className="col-lg-4 p-4 gx-4 gy-4">
-      <div className="Card hoverable shadow">
-        <img
-          src={ad?.photos?.[0].Location}
-          alt={`${ad?.type}-${ad?.address}-${ad?.action}-${ad?.price}`}
-          style={{ height: "250px", objectFit: "cover" }}
-        />
-      </div>
-      <div className="card-body">
-        <h3>CAD&nbsp;{format(ad?.price)}</h3>
-        <p>{ad?.address}</p>
+      <Badge.Ribbon
+        text={`${ad?.type} for ${ad?.action}`}
+        color={`${ad?.action==="Sell"?"red" :"blue"}`}
+      >
+        <div className="Card hoverable shadow">
+          <img
+            src={ad?.photos?.[0].Location}
+            alt={`${ad?.type}-${ad?.address}-${ad?.action}-${ad?.price}`}
+            style={{ height: "250px", objectFit: "cover" }}
+          />
+        </div>
+        <div className="card-body">
+          <h3>CAD&nbsp;{format(ad?.price)}</h3>
+          <p>{ad?.address}</p>
 
-        <p className="card-text d-flex justify-content-between">
-          {ad?.bedrooms ? (
-            <span>
-              <IoBedOutline /> {ad?.bedrooms}
-            </span>
-          ) : (
-            ""
-          )}{" "}
-          {ad?.bathrooms ? (
-            <span>
-              <TbBath /> {ad?.bathrooms}
-            </span>
-          ) : (
-            ""
-          )}{" "}
-          {ad?.landsize ? (
-            <span>
-              <BiArea /> {ad?.landsize}
-            </span>
-          ) : (
-            ""
-          )}{" "}
-        </p>
-      </div>
+          <p className="card-text d-flex justify-content-between">
+            {ad?.bedrooms ? (
+              <span>
+                <IoBedOutline /> {ad?.bedrooms}
+              </span>
+            ) : (
+              ""
+            )}{" "}
+            {ad?.bathrooms ? (
+              <span>
+                <TbBath /> {ad?.bathrooms}
+              </span>
+            ) : (
+              ""
+            )}{" "}
+            {ad?.landsize ? (
+              <span>
+                <BiArea /> {ad?.landsize}
+              </span>
+            ) : (
+              ""
+            )}{" "}
+          </p>
+        </div>
+      </Badge.Ribbon>
     </div>
   );
 }
