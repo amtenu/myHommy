688f40fb9963917f3e447c9e61ed2bd23551b22d
amtenu <amannov21@gmail.com>
2023-12-29 19:35:51 -0700

    user can see a single ad with it's related ad

--- client/src/pages/AdView.js
+++ client/src/pages/AdView.js
@@ -1,6 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function AdView() {
+  //The state
+
+  const [ad, setAd] = useState({});
+  const [related, setRelated] = useState([]);
+
+  //Hooks
+
   const params = useParams();
 
-  return <>{params.slug}</>;
+  useEffect(() => {
+    if (params?.slug) fetchAd();
+  }, [params?.slug]);
+
+  const fetchAd = async () => {
+    try {
+      const { data } = await axios.get(`/ad/${params.slug}`);
+      setAd(data?.ad)
+      setRelated(data?.related)
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <><pre>{JSON.stringify({ad,related},null,4)}</pre></>;
 }
