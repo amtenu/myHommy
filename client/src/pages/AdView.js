adb69832c68a3fbed06e79892609ed89008c1c4d
amtenu <amannov21@gmail.com>
2024-01-03 12:48:42 -0700

    added and tested react photo gallary

--- client/src/pages/AdView.js
+++ client/src/pages/AdView.js
@@ -1,7 +1,24 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Gallery from 'react-photo-gallery'
+
+const photos = [
+  {
+    src: 'https://myhommy-bucket.s3.ca-central-1.amazonaws.com/dnlUeznBaQlHUr8OQkO7g.jpeg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://myhommy-bucket.s3.ca-central-1.amazonaws.com/dnlUeznBaQlHUr8OQkO7g.jpeg',
+    width: 1,
+    height: 1
+  }
+];
+
 export default function AdView() {
+
+
   //The state
 
   const [ad, setAd] = useState({});
@@ -27,7 +44,9 @@ export default function AdView() {
 
   return (
     <>
+    <Gallery  photos={photos}/>
       <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
+
     </>
   );
 }
