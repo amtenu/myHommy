716d12a7c1abdf8a4d1cc536e976473f28826511
amtenu <amannov21@gmail.com>
2024-01-03 21:49:24 -0700

    separted the function to its own component

--- client/src/components/cards/AdCard.js
+++ client/src/components/cards/AdCard.js
@@ -1,11 +1,10 @@
 import { Badge } from "antd";
 import { Link } from "react-router-dom";
 import AdFeatures from "./AdFeatures";
+import {format} from "../../helpers/ad"
 
 export default function AdCard({ ad }) {
-  function format(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+
   return (
     <div className="col-lg-4 p-4 gx-4 gy-4">
       <Link to={`/ad/${ad.slug}`}>
