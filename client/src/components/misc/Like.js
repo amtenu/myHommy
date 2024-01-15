20a5dad520ed808e1d2c37241702e0d372546195
amtenu <amannov21@gmail.com>
2024-01-15 16:27:57 -0700

    user can like a property and data is saved as wishlist

--- client/src/components/misc/Like.js
+++ client/src/components/misc/Like.js
@@ -1,19 +1,49 @@
 import { useAuth } from "../../context/auth";
 import { FcLike } from "react-icons/fc";
 import { FcLikePlaceholder } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Like({ ad }) {
   const [auth, setAuth] = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLike = async () => {
+    try {
+      if (auth.user === null) {
+        navigate("/login");
+        return;
+      }
+      const { data } = await axios.post("/wishlist", { adId: ad._id });
+      setAuth({ ...auth, user: data });
+      const fromLs = JSON.stringify(localStorage.getItem("auth"));
+      fromLs.user = data;
+      localStorage.setItem("auth", JSON.stringify(fromLs));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       {auth.user?.wishlist?.includes(ad?._id) ? (
         <span>
-           <FcLike className="h2 mt-3"/>
+          <FcLike onClick={handleLike} className="h2 mt-3 pointer" />
         </span>
       ) : (
         <span>
-         
-          <FcLikePlaceholder className="h2 mt-3" />
+          <FcLikePlaceholder
+            onClick={handleUnlike}
+            className="h2 mt-3 pointer"
+          />
         </span>
       )}
     </>
