0925d397c37bb34339a3d2c7ade8081dae1ab9b0
amtenu <amannov21@gmail.com>
2024-01-16 11:07:26 -0700

    pin instered to user experiance

--- client/src/components/cards/Map.js
+++ client/src/components/cards/Map.js
@@ -1,26 +1,27 @@
 import GoogleMapReact from "google-map-react";
-import { GOOGLE_MAP_PLACES_KEY} from "../../config/config";
+import { GOOGLE_MAP_PLACES_KEY } from "../../config/config";
 export default function Map() {
   //-114.057968,51.04731
-  
-    const defaultProps = {
+
+  const defaultProps = {
     center: {
-        lat:51.04731,
-        lng:-114.057968
-       
+      lat: 51.04731,
+      lng: -114.057968,
     },
     zoom: 11,
   };
 
   return (
     <div style={{ width: "100%", height: "350px" }}>
-      <GoogleMapReact>
+      <GoogleMapReact
         bootstrapURLKeys={{ key: GOOGLE_MAP_PLACES_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-        
+        >
+        <div lat={51.04731} lng={-114.057968}>
+          <span className="lead">📍</span>
+        </div>
       </GoogleMapReact>
-
     </div>
   );
 }
