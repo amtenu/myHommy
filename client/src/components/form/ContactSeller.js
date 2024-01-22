b0da9d131460608be228e96a62f42ee4811d0c08
amtenu <amannov21@gmail.com>
2024-01-22 14:13:43 -0700

    logged in users can view form to send enquiry using the email they want

--- client/src/components/form/ContactSeller.js
+++ client/src/components/form/ContactSeller.js
@@ -17,7 +17,12 @@ export default function ContactSeller({ ad }) {
   // hooks
 
   const navigate = useNavigate();
-console.log (ad)
+
+   const LoggedIn=auth.user !==null &&auth.token !==""
+
+
+
+  console.log(ad);
   return (
     <>
       <div className="row">
@@ -26,6 +31,51 @@ console.log (ad)
             Contact{" "}
             {ad?.postedBy?.name ? ad?.postedBy?.name : ad?.postedBy?.username}
           </h3>
+          <form>
+            <textarea
+              name="message"
+              placeholder="Please write your message here"
+              className="form-control"
+              value={message}
+              autoFocus={true}
+              onChange={(e) => e.target.value}
+              disabled={!LoggedIn}
+            ></textarea>
+
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Please enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              disabled={!LoggedIn}
+            ></input>
+            <input
+              type="text"
+               
+              className="form-control mb-3"
+              placeholder="Please enter your email"
+              value={email}
+              onChange={(e)=>e.target.value}
+              disabled={!LoggedIn}
+              
+            ></input>
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder="Please enter your phone no."
+              value={name}
+              onChange={(e) => setPhone(e.target.value)}
+              disabled={!LoggedIn}
+            ></input>
+
+            <button
+              className="btn btn-primary mb-3 mt-3"
+              disabled={!name || !email || loading}
+            >
+              {LoggedIn ?loading ? "Please wait " : "Send enquiry" :"Login in to send enquiry"}
+            </button>
+          </form>
         </div>
       </div>
     </>
