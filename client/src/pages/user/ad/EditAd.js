56f95b4325f83558b65641e07782010d33218dd3
amtenu <amannov21@gmail.com>
2024-01-26 15:42:31 -0700

    users can see and edit thier ads fully .

--- client/src/pages/user/ad/EditAd.js
+++ client/src/pages/user/ad/EditAd.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { GOOGLE_PLACES_KEY } from "../../../config/config";
 import CurrencyInput from "react-currency-input-field";
@@ -34,11 +34,21 @@ export default function EditAd({ action, type }) {
   const navigate = useNavigate();
   const params = useParams();
 
+  //load the items
+
+  useEffect(() => {
+    if (params?.slug) {
+      //   console.log("params?.slug =>", params?.slug);
+      fetchAds();
+    }
+  }, [params?.slug]);
+
   const handleClick = async () => {
     try {
       setAd({ ...ad, loading: true });
       const { data } = await axios.put(`/ad/${ad._id}`, ad);
-      console.log("Ad create response =>", data);
+      console.log("Show single page data =>", data);
+
       if (data?.error) {
         toast.error(data.error);
         setAd({ ...ad, loading: false });
@@ -52,126 +62,145 @@ export default function EditAd({ action, type }) {
     }
   };
 
+  const fetchAds = async () => {
+    try {
+      const { data } = await axios.get(`/ad/${params.slug}`);
+      //   console.log(data)//data consists of related data as well so need to remove related
+
+      setAd(data?.ad);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-        <h1 className="display-1 bg-primary text-light p-5">  Edit Ad</h1>
-        <Sidebar/>
-    <div className="container">
-      <div className="form-control mb-3">
-        <ImageUpload ad={ad} setAd={setAd} />
-        <GooglePlacesAutocomplete
-          apiKey={GOOGLE_PLACES_KEY}
-          apiOptions="ca"
-          selectProps={{
-            defaultInputValue: ad?.address,
-            placeholder: "Search for address ...",
-            onChange: ({ value }) =>
-              setAd({ ...ad, address: value.description }),
-          }}
-        />
-      </div>
-      <div style={{ marginTop: "80px" }}>
-        <CurrencyInput
-          placeholder="Please Enter price"
-          defaultValue={ad.price}
+      <h1 className="display-1 bg-primary text-light p-5"> Edit Ad</h1>
+      <Sidebar />
+      <div className="container">
+        <div className="form-control mb-3">
+          <ImageUpload ad={ad} setAd={setAd} />
+          {ad?.address ? (
+            <GooglePlacesAutocomplete
+              apiKey={GOOGLE_PLACES_KEY}
+              apiOptions="ca"
+              selectProps={{
+                defaultInputValue: ad?.address,
+                placeholder: "Search for address ...",
+                onChange: ({ value }) =>
+                  setAd({ ...ad, address: value.description }),
+              }}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        <div style={{ marginTop: "80px" }}>
+          {ad?.price ? (
+            <CurrencyInput
+              placeholder="Please Enter price"
+              defaultValue={ad.price}
+              className="form-control mb-3"
+              onValueChange={(value) => setAd({ ...ad, price: value })}
+              required
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        {type === "House" ? (
+          <>
+            {" "}
+            <input
+              type="number"
+              min="0"
+              className="form-control mb-3"
+              placeholder="How many bedrooms?"
+              value={ad.bedrooms}
+              onChange={(e) =>
+                setAd({
+                  ...ad,
+                  bedrooms: e.target.value,
+                })
+              }
+            />
+            <input
+              type="number"
+              min="0"
+              className="form-control mb-3"
+              placeholder="How many bathrooms?"
+              value={ad.bathrooms}
+              onChange={(e) =>
+                setAd({
+                  ...ad,
+                  bathrooms: e.target.value,
+                })
+              }
+            />
+            <input
+              type="number"
+              min="0"
+              className="form-control mb-3"
+              placeholder="How many carparks?"
+              value={ad.carPark}
+              onChange={(e) =>
+                setAd({
+                  ...ad,
+                  carPark: e.target.value,
+                })
+              }
+            />
+          </>
+        ) : (
+          ""
+        )}
+
+        <input
+          type="text"
           className="form-control mb-3"
-          onValueChange={(value) => setAd({ ...ad, price: value })}
-          required
+          placeholder="Land size?"
+          value={ad.landsize}
+          onChange={(e) =>
+            setAd({
+              ...ad,
+              landsize: e.target.value,
+            })
+          }
         />
+
+        <input
+          type="text"
+          className="form-control mb-3"
+          placeholder="Enter title?"
+          value={ad.title}
+          onChange={(e) =>
+            setAd({
+              ...ad,
+              title: e.target.value,
+            })
+          }
+        />
+
+        <textarea
+          className="form-control mb-3"
+          placeholder="Description?"
+          value={ad.description}
+          onChange={(e) =>
+            setAd({
+              ...ad,
+              description: e.target.value,
+            })
+          }
+        />
+        <button
+          onClick={handleClick}
+          className={`btn btn-primary mb-5 ${ad.loading ? "disabled" : ""} `}
+        >
+          {ad.loading ? "Saving ...." : "Submit"}
+        </button>
+
+        {/*<pre>{JSON.stringify(ad, null, 4)}</pre>*/}
       </div>
-      {type === "House" ? (
-        <>
-          {" "}
-          <input
-            type="number"
-            min="0"
-            className="form-control mb-3"
-            placeholder="How many bedrooms?"
-            value={ad.bedrooms}
-            onChange={(e) =>
-              setAd({
-                ...ad,
-                bedrooms: e.target.value,
-              })
-            }
-          />
-          <input
-            type="number"
-            min="0"
-            className="form-control mb-3"
-            placeholder="How many bathrooms?"
-            value={ad.bathrooms}
-            onChange={(e) =>
-              setAd({
-                ...ad,
-                bathrooms: e.target.value,
-              })
-            }
-          />
-          <input
-            type="number"
-            min="0"
-            className="form-control mb-3"
-            placeholder="How many carparks?"
-            value={ad.carPark}
-            onChange={(e) =>
-              setAd({
-                ...ad,
-                carPark: e.target.value,
-              })
-            }
-          />
-        </>
-      ) : (
-        ""
-      )}
-
-      <input
-        type="text"
-        className="form-control mb-3"
-        placeholder="Land size?"
-        value={ad.landsize}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            landsize: e.target.value,
-          })
-        }
-      />
-
-      <input
-        type="text"
-        className="form-control mb-3"
-        placeholder="Enter title?"
-        value={ad.title}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            title: e.target.value,
-          })
-        }
-      />
-
-      <textarea
-        className="form-control mb-3"
-        placeholder="Description?"
-        value={ad.description}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            description: e.target.value,
-          })
-        }
-      />
-      <button
-        onClick={handleClick}
-        className={`btn btn-primary mb-5 ${ad.loading ? "disabled" : ""} `}
-      >
-        {ad.loading ? "Saving ...." : "Submit"}
-      </button>
-
-      {/*<pre>{JSON.stringify(ad, null, 4)}</pre>*/}
-    </div>
     </div>
   );
 }
