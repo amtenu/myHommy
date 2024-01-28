1faf44f957167deae037c3385981ab24f9ca76b3
amtenu <amannov21@gmail.com>
2024-01-28 06:03:53 -0700

    completed UI/UX of search form based on locationwith options

--- client/src/components/form/SearchForm.js
+++ client/src/components/form/SearchForm.js
@@ -1,12 +1,37 @@
-import { useState } from "react"
-import { useSearch } from "../../context/Search"
-export default function SearchForm(){
-    //context
+import { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { useSearch } from "../../context/Search";
+import { GOOGLE_PLACES_KEY } from "../../config/config";
+export default function SearchForm() {
+  //context
 
-    const [search,setSearch]=useSearch()
-    return (
-        <>
-     search form
-        </>
-    )
+  const [search, setSearch] = useSearch();
+  return (
+    <>
+      <div className="container   m-5">
+        <div className="row">
+          <div className="col-lg-12 form-control">
+            <GooglePlacesAutocomplete
+              apiKey={GOOGLE_PLACES_KEY}
+              apiOptions="ca"
+              selectProps={{
+                defaultInputValue: search?.address,
+                placeholder: "Search for address ...",
+                onChange: ({ value }) =>
+                  setSearch({ ...search, address: value.description }),
+              }}
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button className="btn btn-primary col-lg-2 square">Buy</button>
+          <button className="btn btn-primary col-lg-2 square">Rent</button>
+          <button className="btn btn-primary col-lg-2 square">House</button>
+          <button className="btn btn-primary col-lg-2 square">Land</button>
+          <button className="btn btn-primary col-lg-2 square">Price</button>
+          <button className="btn btn-danger col-lg-2 square">Search</button>
+        </div>
+      </div>
+    </>
+  );
 }
