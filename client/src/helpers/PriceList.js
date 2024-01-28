8dd97432bff74b9ade6e3cbe4fee399b926218b0
amtenu <amannov21@gmail.com>
2024-01-28 06:54:11 -0700

    Search array for the Prices for sell and buy

---
+++ client/src/helpers/PriceList.js
@@ -0,0 +1,54 @@
+const sellPrices = [
+    {
+      _id: 0,
+      name: "All price",
+      array: [0, 10000000],
+    },
+    {
+      _id: 1,
+      name: "$0 to $200,000",
+      array: [0, 200000],
+    },
+    {
+      _id: 2,
+      name: "$200,000 to $500,000",
+      array: [200000, 500000],
+    },
+    {
+      _id: 3,
+      name: "$500,000 to $1,000,000",
+      array: [500000, 1000000],
+    },
+    {
+        _id: 4,
+        name: "$1,000,000 to $10,000,000",
+        array: [500000, 1000000],
+      },
+  ];
+
+
+   
+  const rentPrices = [
+    {
+      _id: 0,
+      name: "All price",
+      array: [0, 10000],
+    },
+    {
+      _id: 1,
+      name: "$0 to $1000",
+      array: [0, 1000],
+    },
+    {
+      _id: 2,
+      name: "$1000 to $5000",
+      array: [1000, 5000],
+    },
+    {
+      _id: 3,
+      name: "$5000 to $10000",
+      array: [5000, 10000],
+    },
+  ];
+   
+  export { sellPrices, rentPrices }
